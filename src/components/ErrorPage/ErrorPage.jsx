4972abab9dced5b39b5bc84067bd1bf5854308ac
nishulabiba <nishlabiba2gmail.com>
2023-04-10 20:32:39 +0600

    category data created

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -6,9 +6,9 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+      <h1 className="text-center text-8xl m-8">Oops!</h1>
+      <p className="text-4xl text-center">Sorry, an unexpected error has occurred. ^_^</p>
+      <p className="text-xl text-center">
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
